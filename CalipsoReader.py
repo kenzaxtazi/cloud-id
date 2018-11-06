a022e9d5938f9f922481ec2b2f10c2fb2dcffc6f
trz228 <37194706+trz228@users.noreply.github.com>
2018-11-06 16:39:49 +0000

    Added support for unpacking (n, 3) arrays

--- CalipsoReader.py
+++ CalipsoReader.py
@@ -16,7 +16,7 @@ from subprocess import Popen, PIPE
 variables1km = {1:'lat', 2:'lon', 22:'num_layers', 50:'feature_classification_flags'}
 variables5km = {1: 'lat', 2:'lon', 69:'feature_classification_flags', 76:'feature_optical_depth'}
 print('f')
-index = '1'
+index = '76'
 
 filename = "D:/Calipso/5km/CAL_LID_L2_05kmCLay-Standard-V4-20.2018-03-31T23-18-17ZN.hdf"
 try:
@@ -29,23 +29,36 @@ except(FileNotFoundError):
             a = proc.stdout.read()
             print('f')
 
+def load1var5km():
+    b = str(a)
+    c = b[2:-14]
+    d = c.split(" ")
+    for i in range(3, len(d), 3):
+        d[i] = d[i][4:]
+    e = np.float64(d)
+    e = e.reshape(int(len(e)/3), 3) 
+    return(e)
 
-# Process data into float array
-b = str(a)
-c = b.split(" \\r\\n")
-d = np.array(c)
-d[0] = d[0][2:]
-e = d[:-1]
-f = np.float64(e)
+def load1var1km():
+    b = str(a)
+    c = b.split(" \\r\\n")
+    d = np.array(c)
+    d[0] = d[0][2:]
+    e = d[:-1]
+    f = np.float64(e)
+    return(f)
 
-
-#b = str(a)
-#c = b.split(" \\r\\n")
-#d = np.array(c)
-#d[0] = d[0][2:]
-#e = d[:-1]
-#lat = e[:21120]
-#lat = np.float64(lat)
-#lon = e[21120:]
-#lon[0] = lon[0][8:]
-#lon = np.float64(lon)
+def load2var1km():
+    b = str(a)
+    c = b.split(" \\r\\n")
+    d = np.array(c)
+    d[0] = d[0][2:]
+    e = d[:-1]
+    lat = e[:21120]
+    lat = np.float64(lat)
+    lon = e[21120:]
+    lon[0] = lon[0][8:]
+    lon = np.float64(lon)
+    return(lat, lon)
+    
+f = load1var5km()

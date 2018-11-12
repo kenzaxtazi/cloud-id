ac3ba4b87e34e68eadcceec65432e1e3e57f3d7e
trz228 <37194706+trz228@users.noreply.github.com>
2018-11-12 23:04:04 +0000

    Edited collocation function to use linear interpolation

--- Collocation.py
+++ Collocation.py
@@ -77,13 +77,87 @@ def collocate(SLSTR_filename, Calipso_filename):
     clat = CR.load_data(file, 'Latitude')
     clon = CR.load_data(file, 'Longitude')
     
-    # Find coord pairs which are close, - latitude dependence needs to be added
+    # Find coord pairs which are close
     coords = []
+    # Want the latitude / longitude to be within 250m of each others
+    # 250m = 0.00224577793 degrees lon at equator
+    # 250m = 0.00224577793 * cos(lat) degrees lon at lat
+    lontolerance = 0.0224577793 * np.cos(clat[0] * np.pi / 180)
+    lattolerance = 0.0224577793
+    #check edges
+    
+    # Edge flag
+    top, bottom, left, right = False, False, False, False
+    
+    # Top/Bottom Row
+    for i in [0, 2399]:
+        for j in range(3000):
+            matches = np.isclose(slat[i, j], clat, rtol=lattolerance)
+            if matches.any():
+                loc = np.where(matches == True)
+                if np.isclose(slon[i, j], clon[loc[0][0]], rtol=lontolerance):
+                    coords.append([i, j, loc[0][0]])
+                    if i == 0:
+                        top = True
+                    elif i == 2399:
+                        bottom = True
+    
+    # Left/Right Col
+    for i in range(1, 2399):
+        for j in [0, 2999]:
+            matches = np.isclose(slat[i, j], clat, rtol=lattolerance)
+            if matches.any():
+                loc = np.where(matches == True)
+                if np.isclose(slon[i, j], clon[loc[0][0]], rtol=lontolerance):
+                    coords.append([i, j, loc[0][0]])
+                    if j == 0:
+                        left = True
+                    if j == 2999:
+                        right = True
+            
+    if len(coords) == 0:
+        print('No matches found')
+        return(coords)
+    
+    edgeflagsum = top + right + left + bottom
+    if edgeflagsum == 2:
+        # Use interpolation between two edges to find which pixels to check
+        xedge = [i[0] for i in coords]
+        yedge = [i[1] for i in coords]
+        xs = np.linspace(np.min(xedge) + 1, np.max(xedge) - 1, np.max(xedge) - np.min(xedge) - 1).astype(int)
+        ys = np.interp(xs, xedge, yedge)
+        ys = ys.astype(int)
+
+        
+        for i in range(len(xs)):
+            ylist = [ys[i]]
+            if ys[i] != 0:
+                ylist.append(i-1)
+            if ys[i] != 2999:
+                ylist.append(i+1)
+            for y in ylist:
+                matches = np.isclose(slat[xs[i], y], clat, rtol=lattolerance)
+                if matches.any():
+                    loc = np.where(matches == True)
+                    if np.isclose(slon[xs[i], y], clon[loc[0][0]], rtol=lontolerance):
+                        coords.append([xs[i], y, loc[0][0]])
+        
+        return(coords, xedge, yedge)
+        
+    elif edgeflagsum > 2:
+        # Should never happen 
+        print('Warning: More than 2 Edges detected.')
+        
+    elif edgeflagsum < 2:
+        # Occurs if a region is NaN or the Calipso Granule starts/ends in frame
+        print('Warning: Less than 2 Edges detected.')
+        
+    
     for i in tqdm(range(2400)):
         for j in range(3000):
-            if (np.isclose(slat[i, j], clat)).any():
-                loc = np.where(np.isclose(slat[i, j], clat) == True)
-                if np.isclose(slon[i, j], clon[loc[0][0]]):
+            if (np.isclose(slat[i, j], clat, rtol=1e-3)).any():
+                loc = np.where(np.isclose(slat[i, j], clat, rtol=1e-3) == True)
+                if np.isclose(slon[i, j], clon[loc[0][0]], rtol=1e-3):
                     coords.append([i, j, loc[0][0]])
     
     # Return position of matching coordinates in a row

64fde1ea9b8ce9f86cfbbe0a64521670e93b87c3
Tom Zhu <tomzhu1@gmail.com>
2018-11-19 08:46:09 +0000

    Added functions to produce and parse ESA html requests

--- CalipsoReader2.py
+++ CalipsoReader2.py
@@ -32,11 +32,13 @@ def load_data(file, variable):
     return(data)
     
 if __name__ == '__main__':
-    filename = "D:/SatelliteData/Calipso1km/CAL_LID_L2_01kmCLay-Standard-V4-10.2018-04-01T00-04-48ZD.hdf"
+    Cfilename = "D:\SatelliteData\Calipso1kmV3\CAL_LID_L2_01kmCLay-ValStage1-V3-40.2018-10-30T23-43-15ZD.hdf"
 
-    file = SD(filename, SDC.READ)
+    file = SD(Cfilename, SDC.READ)
     
     clat = load_data(file, 'Latitude')
     clon = load_data(file, 'Longitude')
     ctime = load_data(file, 'Profile_Time')
+    ctime += 725846400.0    # Time in UNIX 
+    ctime -= 10 # Leap second correction
     cutctime = load_data(file, 'Profile_UTC_Time')
---
+++ httptest.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 18 15:07:55 2018
+
+@author: tomzh
+"""
+
+import requests
+import xml.etree.ElementTree as ET
+import CalipsoReader2 as CR
+from datetime import datetime, timedelta
+import numpy as np
+
+def SLSTR_query(url):
+    r = requests.get(url, auth=('s3guest', 's3guest'))
+    root = ET.fromstring(r.text)
+    out = []
+    
+    # Number of matches:
+    if 'totalResult' in str(root[5]):
+        num_matches = root[5].text
+    else:
+        num_matches = 0
+        
+    if num_matches == 0:
+        print('No matches')
+        
+    else:
+        for i in root:
+            if "entry" in str(i):
+                out.append(i[0].text + "," + i[1].attrib['href'])
+    print(num_matches)
+    return(out)
+    
+def makeurlquery(Cfilename, timewindow=30, num=20):
+    # Set download website, instrument and product type
+    
+    base = "https://scihub.copernicus.eu/s3//search?q=%20instrumentshortname:SLSTR%20AND%20%20producttype:SL_1_RBT___"
+
+    
+    # Load parameters from hdf file
+    file = CR.load_hdf(Cfilename)
+    lat = CR.load_data(file, 'Latitude')
+    lon = CR.load_data(file, 'Longitude')
+    time = CR.load_data(file, 'Profile_Time')   # Time in IAT
+    time += 725846400.0    # Time in UNIX 
+    time -= 10 # Leap second correction
+    
+    # Select which indices to use to slice list
+    xs = np.linspace(0, len(lat) - 1, num + 1)
+    xs = xs.astype(int)
+    
+    out = []
+    
+    # Set time query
+    for i in range(len(xs) - 1):
+        query = ""
+        a = xs[i]
+        b = xs[i+1]
+        c = int(0.5 * (a + b))
+        timestamp = datetime.utcfromtimestamp(time[c][0])
+        windowstart = timestamp - timedelta(minutes = timewindow)
+        windowend = timestamp + timedelta(minutes = timewindow)
+        query += "%20AND%20%20beginPosition:["
+        query += str(windowstart.isoformat())[:-3] + 'Z'
+        query += "%20TO%20"
+        query += str(windowend.isoformat())[:-3] + 'Z' + "]"
+
+    
+    # Set Positional query
+        query += "%20%20%20AND%20%20(%20footprint:%22Intersects(POLYGON(("
+        query += str(lon[a][0]) + "%20" + str(lat[a][0]) + str(',')
+        query += str(lon[b][0]) + "%20" + str(lat[a][0]) + str(',')
+        query += str(lon[b][0]) + "%20" + str(lat[b][0]) + str(',')
+        query += str(lon[a][0]) + "%20" + str(lat[b][0]) + str(',')
+        query += str(lon[a][0]) + "%20" + str(lat[a][0])
+        query += "%20)))%22)&rows=25&start=0"
+        
+        out.append(base + query)
+    return(out)
+
+
+def find_SLSTR_data(Cfilename, timewindow=30, num=20):
+    out = []
+    queries = makeurlquery(Cfilename, timewindow, num)
+    Sfilenames = []
+    Sdownloads = []
+    for query in queries:
+        response = SLSTR_query(query)
+        if response != []:
+            out += response
+    out = list(set(out))
+    for i in out:
+        q = i.split(',')
+        Sfilenames.append(q[0])
+        Sdownloads(q[1])
+    return(Sfilenames, Sdownloads)
+    
+    
+if __name__ == '__main__':
+    #url = "https://scihub.copernicus.eu/s3//search?q=%20instrumentshortname:SLSTR%20AND%20producttype:SL_1_RBT___%20AND%20(%20footprint:%22Intersects(POLYGON((101.1878500000000%2016.7101170000000,98.9603900000000%2016.7101170000000,98.9603900000000%2026.2178780000000,101.1878500000000%2026.2178780000000,101.1878500000000%2016.7101170000000%20)))%22)&rows=25&start=0"
+    #print(SLSTR_query(url))
+    pass
+    

6facc14b414a38028bdc98f821507db4171b1bca
Tom Zhu <tomzhu1@gmail.com>
2018-11-29 20:08:08 +0000

    Moved functionality into importable function

--- CEDAdownloadmatches.py
+++ CEDAdownloadmatches.py
@@ -7,41 +7,49 @@ Created on Sat Nov 24 16:44:37 2018
 
 import DataLoader as DL
 import os
-from ftplib import FTP
 from tqdm import tqdm
 import zipfile
 
-with open('Matches.txt', 'r') as file:
-    data = file.readlines()
-    
-Sfiles = [i.split(',')[1] for i in data]
+def CEDA_download(MatchesFilename='Matches.txt', SLSTR_target_directory="/vols/lhcb/egede/cloud/SLSTR/2018/04"):
 
-
-ftp = DL.FTPlogin()
-    
-startdir = os.getcwd()
-os.chdir("/vols/lhcb/egede/cloud/SLSTR/2018/04")
-
-q = os.listdir()
-
-Sfiles1 = []
-
-for i in range(len(Sfiles)):
-    if Sfiles[i] + ".SEN3" not in q:
-        path = Sfiles[i]
-        path = path[16:20] + '/' + path[20:22] + '/' + path[22:24] + '/' + path[:] + ".zip"
-        Sfiles1.append(path)
+    with open('Matches.txt', 'r') as file:
+        data = file.readlines()
         
-for i in tqdm(range(len(Sfiles1))):
-    targetfile = Sfiles1[i]
-    downloadedfile = str(Sfiles[i] + ".zip")
-    tqdm.write('Downloading' + str(targetfile))
-    try:
-        ftp.retrbinary("RETR " + targetfile, open(downloadedfile, "wb").write)
-        z = zipfile.ZipFile(downloadedfile)
-        z.extractall()
-        os.remove(downloadedfile)
-    except:
-        print('Error downloading' + file)
+    Sfiles = [i.split(',')[1] for i in data]
+    
+    
+    ftp = DL.FTPlogin()
         
-os.chdir(startdir)
+    startdir = os.getcwd()
+    os.chdir(SLSTR_target_directory)
+    
+    q = os.listdir()
+    
+    Sfiles1 = []
+    
+    for i in range(len(Sfiles)):
+        if Sfiles[i] + ".SEN3" not in q:
+            path = Sfiles[i]
+            path = path[16:20] + '/' + path[20:22] + '/' + path[22:24] + '/' + path[:] + ".zip"
+            Sfiles1.append(path)
+            
+    for i in tqdm(range(len(Sfiles1))):
+        targetfile = Sfiles1[i]
+        downloadedfile = str(Sfiles[i] + ".zip")
+        tqdm.write('Downloading' + str(targetfile))
+        try:
+            ftp.retrbinary("RETR " + targetfile, open(downloadedfile, "wb").write)
+            z = zipfile.ZipFile(downloadedfile)
+            z.extractall()
+            os.remove(downloadedfile)
+        except:
+            tqdm.write('Error downloading' + file)
+            try:
+                os.remove(downloadedfile)
+            except FileNotFoundError:
+                pass
+            
+    os.chdir(startdir)
+    
+if __name__ == "__main__":
+    CEDA_download()

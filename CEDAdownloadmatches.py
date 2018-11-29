c75b5c4d6443f78522ad3a5ee224cceca345ec01
Tom Zhu <tomzhu1@gmail.com>
2018-11-29 20:26:07 +0000

    Made file pep8 compliant

--- CEDAdownloadmatches.py
+++ CEDAdownloadmatches.py
@@ -10,40 +10,42 @@ import os
 from tqdm import tqdm
 import zipfile
 
+
 def CEDA_download(MatchesFilename='Matches.txt', SLSTR_target_directory="/vols/lhcb/egede/cloud/SLSTR/2018/04"):
 
     with open('Matches.txt', 'r') as file:
         data = file.readlines()
-        
+
     Sfiles = [i.split(',')[1] for i in data]
     Sfiles = list(set(Sfiles))
-    
+
     ftp = DL.FTPlogin()
-        
+
     startdir = os.getcwd()
     os.chdir(SLSTR_target_directory)
-    
+
     q = os.listdir()
-    
+
     Sfiles1 = []
-    
+
     for i in range(len(Sfiles)):
         if Sfiles[i] + ".SEN3" not in q:
             Sfiles1.append(Sfiles[i])
-            
+
     Sfiles2 = []
-    
+
     for i in tqdm(range(len(Sfiles1))):
         path = Sfiles1[i]
         path = path[16:20] + '/' + path[22:24] + '/' + path[:] + '.zip'
         Sfiles2.append(path)
-            
+
     for i in tqdm(range(len(Sfiles2))):
         targetfile = Sfiles2[i]
         downloadedfile = str(Sfiles1[i] + ".zip")
         tqdm.write('Downloading' + str(targetfile))
         try:
-            ftp.retrbinary("RETR " + targetfile, open(downloadedfile, "wb").write)
+            ftp.retrbinary("RETR " + targetfile,
+                           open(downloadedfile, "wb").write)
             z = zipfile.ZipFile(downloadedfile)
             z.extractall()
             os.remove(downloadedfile)
@@ -53,8 +55,9 @@ def CEDA_download(MatchesFilename='Matches.txt', SLSTR_target_directory="/vols/l
                 os.remove(downloadedfile)
             except FileNotFoundError:
                 pass
-            
+
     os.chdir(startdir)
-    
+
+
 if __name__ == "__main__":
     CEDA_download()

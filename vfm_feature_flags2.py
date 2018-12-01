2983ecd38c831b5a23e85d55b7668bdf86e79de8
Tom Zhu <tomzhu1@gmail.com>
2018-12-01 17:13:20 +0000

    Commented out unnecessary code

--- vfm_feature_flags2.py
+++ vfm_feature_flags2.py
@@ -8,20 +8,16 @@ Created on Thu Nov  8 11:48:30 2018
 
 # IDL code
 
-
-import numpy as np
-
-
 def vfm_feature_flags(val):
     """ Python version of the IDL code to read the bitwise flags"""
 
     feature_type = val & 7
-    feature_type_qa = (val >> 3) & 3
-    ice_water_phase = (val >> 5) & 3
-    ice_water_phase_qa = (val >> 7) & 3
-    feature_subtype = (val >> 9) & 7
-    cloud_aerosol_psc_type_qa = (val >> 12) & 1
-    horizontal_averaging = (val >> 13) & 7
+    # feature_type_qa = (val >> 3) & 3
+    # ice_water_phase = (val >> 5) & 3
+    # ice_water_phase_qa = (val >> 7) & 3
+    # feature_subtype = (val >> 9) & 7
+    # cloud_aerosol_psc_type_qa = (val >> 12) & 1
+    # horizontal_averaging = (val >> 13) & 7
 
     # if feature_type == 0:
     #     print("Feature Type : invalid (bad or missing data)")

57bd2f5c529e04e349f4bd383e3ae227934ed278
Kenza Tazi <kt2015@ic.ac.uk>
2018-12-17 15:56:42 +0000

    changed some file names, updated the application one to plot associated SLSTR masks

---
+++ ModelApplication.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  2 12:54:58 2018
+
+@author: kenzatazi
+"""
+
+from satpy import Scene
+from glob import glob
+import matplotlib.pyplot as plt
+import numpy as np 
+from tqdm import tqdm
+
+# create readers and open files
+#scn = Scene(filenames=glob('/home/hep/kt2015/cloud/SLSTR/2018/08/S3A_SL_1_RBT____20180823T041605_20180823T041905_20180824T083800_0179_035_033_1620_LN2_O_NT_003.SEN3/*'), 
+#           reader='nc_slstr')
+
+def apply_mask(model, scenes, bayesian=False, empirical=False):
+
+    """
+    Function to plot the model mask superimposed one or more SLSTR scenes and, 
+    optionallly,  the associated baseyian and empircal cloud masks that come 
+    with the file.
+    
+    Args:
+        model: a tensorflow model object 
+        scenes: a list of satpy Scene objects
+    Kwargs:
+        bayesian=False, if set to True the function will also plot the Bayesian
+                        mask
+        empirical=False,  if set to True the function will also plot the 
+                        empirical mask
+    Returns: 
+        Plot of scenes with masks
+    """    
+    
+    for scn in scenes: 
+
+        scn.load(['S1_an','S2_an','S3_an','S4_an','S5_an','S6_an','S7_in','S8_in',
+                  'S9_in','bayes_an', 'bayes_in','cloud_an', 'longitude_an', 
+                  'latitude_an', 'satellite_zenith_angle', 'solar_zenith_angle'])
+        
+        S1= np.nan_to_num(scn['S1_an'].values)
+        S2= np.nan_to_num(scn['S2_an'].values)
+        S3= np.nan_to_num(scn['S3_an'].values)
+        S4= np.nan_to_num(scn['S4_an'].values)
+        S5= np.nan_to_num(scn['S5_an'].values)
+        S6= np.nan_to_num(scn['S6_an'].values)
+        S7= np.nan_to_num(scn['S7_in'].values) 
+        S8= np.nan_to_num(scn['S8_in'].values)
+        S9= np.nan_to_num(scn['S9_in'].values)
+        salza =  np.nan_to_num(scn['satellite_zenith_angle'].values)
+        solza =  np.nan_to_num(scn['solar_zenith_angle'].values)
+        lat =  np.nan_to_num(scn[ 'latitude_an'].values)
+        lon =  np.nan_to_num(scn['longitude_an'].values)
+        
+        mask= []
+        
+        for i in tqdm(range(len(S1))):
+            for j in range(len(S1[0])):
+                inputs = np.array([S1[i,j],S2[i,j],S3[i,j],S4[i,j],S5[i,j],S6[i,j],
+                                  S7[int(i/2.),int(j/2.)],S8[int(i/2.),int(j/2.)],
+                                  S9[int(i/2.),int(j/2.)],salza[int(i/2.),int(j/2.)],
+                                  solza[int(i/2.),int(j/2.)],
+                                  lat[i,j],lon[i,j]])
+                inputs = inputs.reshape(-1,1,13,1)
+                label =  model.predict_label(inputs)
+                mask.append(label)
+        
+        mask = (np.array(mask)[:,:,0]).reshape(2400,3000)
+        
+        bayes_mask=[]
+        for bitmask in scn['bayes_in'].flag_masks[:-2]:
+            data = scn['bayes_in'].values & bitmask
+            bayes_mask.append(data)
+        bayes_mask= np.sum(bayes_mask, axis=0)
+        
+        plt.figure()
+        plt.imshow(S1, 'gray')
+        plt.imshow(mask, vmax=1, cmap='Blues', alpha=0.3)
+        plt.imshow(bayes_mask, cmap='OrRd', alpha=0.3)
+    plt.show()
---
+++ ModelEvaluation.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 19 14:37:40 2018
+
+@author: kenzatazi
+"""
+
+# Evaluating the model 
+
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import tensorflow as tf 
+import tflearn
+import numpy as np 
+
+
+
+def get_accuracy(model,validation_data,validation_truth):
+    accuracy = model.evaluate(validation_data, validation_truth)
+    return accuracy[0]
+    
+    
+    
+def accuracy_vs_inputs(model, training_data, validation_data, training_truth, 
+                       validation_truth):
+    inputs= np.linspace(0,len(training_data),1000)
+    accuracies=[]
+    
+    for i in inputs: 
+        model.fit(training_data[:i], training_truth[:i], n_epoch=1, 
+                  validation_set = (validation_data, validation_truth), 
+                  snapshot_step=1000, show_metric=False)
+        
+        acc=get_accuracy(model,validation_data,validation_truth)
+        accuracies.append(acc)
+   
+    plt.figure('Accuracy vs inputs')
+    plt.title ('Accuracy as a function of the number of inputs')
+    plt.plot(inputs, accuracies)
+    plt.xlabel('Inputs')
+    plt.ylabel('Accuracy')
+
+
+
+def ROC_curve(model,validation_data,validation_truth):
+    
+    """Plots Receiver Operating Characteristic (ROC) curve"""
+
+    predictions = model.predict(validation_data)
+    
+    false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(
+    validation_truth[:,1], predictions[:,1], pos_label=1)
+    
+    plt.figure('ROC curve')
+    plt.title ('ROC curve')
+    plt.plot(false_positive_rate, true_positive_rate)
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.plot([0, 1], [0, 1], label="random classifier")
+
+
+def AUC(model,validation_data,validation_truth):
+    
+    predictions = model.predict(validation_data)
+    
+    auc= metrics.roc_auc_score(validation_truth[:,1], predictions[:,1])
+    return auc
+
+
+def precision_vs_recall(model,validation_data,validation_truth):
+
+    predictions = np.nan_to_num(model.predict(validation_data))
+    
+    precision, recall, thresholds = metrics.precision_recall_curve(
+            validation_truth[:,1], predictions[:,1], pos_label=1)
+    
+    plt.figure('Precision vs recall curve')
+    plt.title ('Precision vs recall curve')
+    plt.plot(precision, recall)
+    plt.xlabel('Precision')
+    plt.ylabel('Recall')
+
+
+def confusion_matrix(model,validation_data,validation_truth):
+    """ Returns a confusion matrix"""   
+    labels= model.predict_label(validation_data)
+    confusion_matrix = tf.confusion_matrix(validation_truth[:,1], 
+                                           labels[:,1])
+    with tf.Session().as_default() as sess:
+        m = tf.Tensor.eval(confusion_matrix,feed_dict=None, session=sess)
+    return m 
+    
+
+
+
+

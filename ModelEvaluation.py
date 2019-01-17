c7deaa1038cc25defc4561f8d39eb65ad10e3d9a
Kenza Tazi <kt2015@ic.ac.uk>
2019-01-17 17:49:50 +0000

    plot the Bayesian false positive vs true positive rates on the ROC as a point

--- ModelEvaluation.py
+++ ModelEvaluation.py
@@ -10,7 +10,6 @@ Created on Mon Nov 19 14:37:40 2018
 
 import matplotlib.pyplot as plt
 from sklearn import metrics
-import tensorflow as tf
 import tflearn
 import numpy as np
 
@@ -19,25 +18,51 @@ def get_accuracy(model, validation_data, validation_truth):
 
     """ returns model accuracy """
 
+    para_num = len(validation_data[0])
+
+    validation_data = np.concatenate(validation_data)
+    validation_data = validation_data.reshape(-1, 1, para_num, 1)
+
+    validation_truth = np.concatenate(validation_truth)
+    validation_truth = validation_truth.reshape(-1, 2)
+
     accuracy = model.evaluate(validation_data, validation_truth)
+
     return accuracy[0]
 
 
-def ROC_curve(model, validation_data, validation_truth):
+def ROC_curve(model, validation_data, validation_truth, bayes_mask=None,
+              name=None):
 
     """Plots Receiver Operating Characteristic (ROC) curve"""
+
+    para_num = len(validation_data[0])
+    validation_data = np.concatenate(validation_data)
+    validation_data = validation_data.reshape(-1, 1, para_num, 1)
+    validation_truth = np.concatenate(validation_truth)
+    validation_truth = validation_truth.reshape(-1, 2)
 
     predictions = model.predict(validation_data)
 
     false_positive_rate, true_positive_rate, thresholds = metrics.roc_curve(
             validation_truth[:, 1], predictions[:, 1], pos_label=1)
 
-    plt.figure('ROC curve')
-    plt.title('ROC curve')
+    if name is None:
+        plt.figure('ROC curve')
+        plt.title('ROC curve')
+    else:
+        plt.figure(name + ' ' + 'ROC curve')
+        plt.title(name + ' ' + 'ROC curve')
     plt.plot(false_positive_rate, true_positive_rate)
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
     plt.plot([0, 1], [0, 1], label="random classifier")
+
+    if bayes_mask is not None:
+        false_positive_rate2, true_positive_rate2, thresholds2\
+            = metrics.roc_curve(validation_truth[:, 1],
+                                bayes_mask, pos_label=1)
+        plt.scatter(false_positive_rate2, true_positive_rate2)
 
 
 def AUC(model, validation_data, validation_truth):

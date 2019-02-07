8ccfcf56e2fac2d79ac05ce88f0521b92c30958f
Tom Zhu <tomzhu1@gmail.com>
2019-02-07 00:41:24 +0000

    Fixed syntax error in predict_file

--- SuperModel.py
+++ SuperModel.py
@@ -9,29 +9,29 @@ class SuperModel():
         self.FFN1 = FFN1
         self.FFN2 = FFN2
         self.CNN = CNN
-    
+
     def predict_file(self, Sreference):
 
-        scene = DL.scene_loader(Sreference)
+        ftestdata = dp.getinputs(Sreference, num_inputs=24)  # include indices
+        indices = np.arange(7200000)
 
-        ftestdata = dp.getinputs(scene, num_inputs=24, indices=True) #include indices
-        indices = ftestdata[-1]
+        # exclude indices when predicting
+        predictions1 = self.FFN1.Predict(ftestdata)
+        augm_data = np.column_stack((predictions1, indices))  # merge with indices
 
-        predictions1 = self.FFN1.Predict(ftestdata[:-1]) #exclude indices when predicting 
-        augm_data = np.column_stack((predictions1, indices)) # merge with indices
+        gooddata = [x for x in augm_data if not(0.4 <= x[0] < 0.6)]
+        poordata = [x for x in augm_data if (0.4 <= x[0] < 0.6)]
 
-        gooddata = [x for x in augm_data if not(0.4 <=x[0] <0.6)]
-        poordata = augm_data[0.4 <= augm_data[:, 0] < 0.6]
-        
-        ctestdata = dp.context_getinputs(scene, poordata)  
-        predictions2 = self.CNN.predict(ctestdata[0])   # May require .model.predict
+        ctestdata = dp.context_getinputs(scene, poordata)
+        predictions2 = self.CNN.predict(
+            ctestdata[0])   # May require .model.predict
 
         predictions3 = self.FFN2.predict(
-            np.column_stack((augm_data[:,0], predictions2)))
+            np.column_stack((augm_data[:, 0], predictions2)))
 
-        fixeddata= np.column_stack((predictions3, poordata[:,-1]))
+        fixeddata = np.column_stack((predictions3, poordata[:, -1]))
 
         final_predictions_with_indices = np.concatenate(gooddata, fixeddata)
-        final_predictions = (np.sort(final_predictions_with_indices))[:,0]
+        final_predictions = (np.sort(final_predictions_with_indices))[:, 0]
 
         return final_predictions

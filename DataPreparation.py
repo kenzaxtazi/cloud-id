2988ec514227408977c197cc7cc1f07301d67426
Kenza Tazi <kt2015@ic.ac.uk>
2019-02-09 13:11:33 +0000

    fixing bugs

--- DataPreparation.py
+++ DataPreparation.py
@@ -247,12 +247,8 @@ def cnn_prep_data(location_directory, context_directory, validation_frac=0.15):
         cdf = cdf.values
 
         for i in ldf:
-            print((cdf[:, 0]))
-            print(((cdf[:, 0])[0])[0])
             star_row = cdf[((cdf[:, 0])[0])[0] == i[0]]
-            print('i[1]', i[1])
-            print('star_row[:,1]', star_row[:,1])
-            star_column = star_row[(star_row[:, 0])[1] == i[1]]
+            star_column = star_row[((star_row[:, 0])[0])[1] == i[1]]
             star = star_column[2]
             padded_star= star_padding(star)
             data.append(padded_star)

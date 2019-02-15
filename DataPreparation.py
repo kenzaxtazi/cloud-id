54a64375eb676c47ceb43155caf1e0e63512d2b6
Kenza Tazi <kt2015@ic.ac.uk>
2019-02-15 15:34:43 +0000

    testing

--- DataPreparation.py
+++ DataPreparation.py
@@ -242,9 +242,6 @@ def cnn_prep_data(location_directory, context_directory, validation_frac=0.15):
     stars = c4[:, 2]
     padded_star = star_padding(stars)
 
-    interpolated_padded_stars = (
-        pd.Series(padded_star).interpolate(method='linear')).values
-
     print('matching datasets')
 
     Cpos = C4['Pos'].values
@@ -564,8 +561,8 @@ def star_padding(stars):
         for arm in star:
 
             # keeps track of the arm so we know which arm should be duplicated if needed
-            arm_number += 1 
-            
+            arm_number += 1
+
             if len(arm) < 50:
 
                 if len(arm) > 0:
@@ -586,18 +583,23 @@ def star_padding(stars):
                 if duplicate == True:
                     padded_star.append(padded_arm)
                     duplicate = False
-            
+
             if len(padded_arm) != 50:
-                print(len(padded_arm))
+                print(len(arm))
 
         if arm_number < 8:
             print('arm_number', arm_number)
 
         padded_stars.append(padded_star)
 
-    padded_stars = np.concatenate(padded_stars).reshape((-1, 8, 50, 1))
+    padded_stars = np.concatenate(padded_stars).reshape((-1))
 
-    return padded_stars
+    interpolated_stars = (
+        pd.Series(padded_star).interpolate(method='linear')).values
+
+    shaped_stars = interpolated_stars.reshape((-1, 8, 50, 1))
+
+    return shaped_stars
 
 # Class to add useful methods to pd DataFrame
 

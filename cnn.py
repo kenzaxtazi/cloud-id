a9547361f5793c91e0e5de6013997f0397004281
Tom Zhu <tomzhu1@gmail.com>
2019-02-17 16:51:49 +0000

    Updated cnn functionality

--- cnn.py
+++ cnn.py
@@ -6,16 +6,13 @@
 ##############################################
 
 import datetime
-import os
-import pandas
+
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import dropout, fully_connected, input_data
 from tflearn.layers.estimator import regression
 
-import DataLoader as DL
 import DataPreparation as dp
-import ModelApplication as app
 
 
 class CNN():
@@ -28,17 +25,17 @@ class CNN():
         self.isLoaded = False
         self.img_length = img_length
         self.img_width = img_width
+        self._model = None
+        self._network = None
+        self.run_id = None
 
-    def networkSetup(self):
-        """Setup network for the model. Specify network configuration by setting the networkConfig attribute"""
-        if self.networkConfig is None:  # No network configuration specified
-            self.Network0()  # Use default network
-        else:
-            # Use network function specified by networkConfig
-            networkFunc = getattr(self, self.networkConfig)
-            networkFunc()
+    def __str__(self):
+        out = ('Model: ' + self.name + '\n'
+               + 'Network type: ' + str(self.networkConfig) + '\n'
+               + 'Context shape: ' + str(self.img_length) + ',' + self.img_width)
+        return(out)
 
-    def Network0(self):
+    def NetworkA(self):
         convnet = input_data(
             shape=[None, self.img_length, self.img_width, 1], name='input')
 
@@ -77,75 +74,76 @@ class CNN():
         # gives the paramaters to optimise the network
         self.network = regression(softmax, optimizer='Adam', learning_rate=self.LR,
                                   loss='categorical_crossentropy', name='targets')
-        self.networkConfig = 'cnn'
+        self.networkConfig = 'NetworkA'
 
-    def Setup(self):
-        self.model = tflearn.DNN(
+    @property
+    def network(self):
+        if self._network is not None:
+            return self._network
+
+        if self.networkConfig is None:
+            print('Using default network configuration, Network0')
+            self.NetworkA()
+        else:
+            # Use network function specified by networkConfig
+            networkFunc = getattr(self, self.networkConfig)
+            networkFunc()
+        return self._network
+
+    @property
+    def model(self):
+        if self._model:
+            return self._model
+
+        self._model = tflearn.DNN(
             self.network, tensorboard_verbose=0, tensorboard_dir='./Temp/tflearn_logs')
 
-    def Train(self, training_data, training_truth, validation_data, validation_truth):
+        return self._model
+
+    def Train(self, training_data, training_truth, validation_data, validation_truth, n_epoch=100):
         timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        MODEL_NAME = 'Models/CNN' + timestamp
-        self.model.fit(training_data, training_truth, n_epoch=100,
+        self.run_id = 'Models/' + str(self.name) + '_' + timestamp
+        self.model.fit(training_data, training_truth, n_epoch=n_epoch,
                        validation_set=(validation_data, validation_truth),
-                       snapshot_step=10000, show_metric=True, run_id=MODEL_NAME)
+                       snapshot_step=10000, show_metric=True, run_id=self.run_id)
         self.isLoaded = True
 
     def Save(self):
         self.model.save("Models/" + self.name)
         with open("Models/" + self.name + '.txt', 'w') as file:
-            file.write(self.networkConfig)
+            file.write(self.networkConfig + '\n')
+            file.write(str(self.img_length))
+            file.write(str(self.img_width))
+            file.write(self.run_id)
 
-    def Load(self):
+    def Load(self, verbose=True):
+        if self.isLoaded:
+            raise AssertionError(
+                'Graph already loaded. Consider loading into new object.')
+
         with open('Models/' + self.name + '.txt', 'r') as file:
-            self.networkConfig = file.read()
-            print(self.networkConfig)
-        self.networkSetup()
-        self.Setup()
+            settings = file.readlines()
+            self.networkConfig = settings[0].strip()
+            self.img_length = int(settings[1].strip())
+            self.img_width = int(settings[2].strip())
+
         self.model.load('Models/' + self.name)
         self.isLoaded = True
+        if verbose:
+            print('##############################################')
+            print('Loading successful')
+            print('Model: ' + self.name)
+            print('Network type: ' + self.networkConfig)
+            print('Context shape: ' + str(self.img_length) + ',' + self.img_width)
+            print('##############################################')
 
 
 if __name__ == '__main__':
-    # Pixel Loading
-
-    if os.path.exists('/vols/lhcb/egede/cloud'):
-        # Script is running on lx02
-        scenes = ['/vols/lhcb/egede/cloud/SLSTR/pacific_day/S3A_SL_1_RBT____20171120T190102_20171120T190402_20171122T003854_0179_024_341_2880_LN2_O_NT_002.SEN3',
-                  '/vols/lhcb/egede/cloud/SLSTR/pacific_day/S3A_SL_1_RBT____20171217T190102_20171217T190402_20171218T223735_0179_025_341_2879_LN2_O_NT_002.SEN3',
-                  '/vols/lhcb/egede/cloud/SLSTR/pacific_day/S3A_SL_1_RBT____20180113T190102_20180113T190402_20180114T230219_0179_026_341_2880_LN2_O_NT_002.SEN3',
-                  '/vols/lhcb/egede/cloud/SLSTR/pacific_day/S3A_SL_1_RBT____20180209T190102_20180209T190402_20180210T234449_0179_027_341_2880_LN2_O_NT_002.SEN3']
-
-    if os.path.exists('/Users/kenzatazi'):
-        # Script is running on Kenza's laptop
-        scenes = ['/Users/kenzatazi/Desktop/SatelliteData/S3A_SL_1_RBT____20180529T113003_20180529T113303_20180530T154711_0179_031_351_1620_LN2_O_NT_003.SEN3']
-
-    if os.path.exists('D:'):
-        scenes = []
 
     df = dp.PixelLoader('./SatelliteData/SLSTR/Pixels5')
 
-    # one month at a time
-    training_data, validation_data, training_truth, validation_truth = df.dp.get_cnn_training_data()
+    tdata, vdata, ttruth, vtruth = df.dp.get_cnn_training_data()
 
-    # MACHINE LEARNING MODEL
-
-    # Creating network and setting hypermarameters for model
-
-    timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    MODEL_NAME = 'Models/cnn_' + timestamp
-
-    model = CNN('CNN_Net0', 'Network0')
-    model.networkSetup()
-    model.Setup()
-    model.Train(training_data, training_truth,
-                validation_data, validation_truth)
+    model = CNN('CNN_NetA', 'NetworkA')
+    model.Train(tdata, ttruth, vdata, vtruth)
     model.Save()
-
-    mask1 = app.apply_mask(model.model, scenes, modeltype='cnn')[1]
-
-    # bmask = DL.extract_mask(Sfile, 'cloud_an', 64)
-    bmask = DL.extract_mask(scenes, 'bayes_in', 2)
-
-    # Vis.MaskComparison(Sfile, mask1, bmask, True, 1000)
-    # Vis.simple_mask(mask1, S1)

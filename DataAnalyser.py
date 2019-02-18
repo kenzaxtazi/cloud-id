1b6ae6809526e1f83509ab5add7031ae66d351c6
Kenza Tazi <kt2015@ic.ac.uk>
2019-02-18 15:24:17 +0000

    correction for acc functions

--- DataAnalyser.py
+++ DataAnalyser.py
@@ -211,7 +211,7 @@ class DataAnalyser():
         self._obj.dp.shuffle_by_file(seed)
         self._obj.dp.remove_night()
 
-        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data(seed=seed)
+        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data(seed=seed, input_type=para_num)
 
         times = self._obj['TimeDiff']
         time_array = times.values
@@ -271,7 +271,7 @@ class DataAnalyser():
         self._obj.dp.shuffle_by_file(seed)
         self._obj.dp.remove_night()
 
-        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data()
+        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data(seed=seed, input_type=para_num)
 
         angle_slices = np.linspace(3, 55, 18)
         accuracies = []
@@ -319,7 +319,7 @@ class DataAnalyser():
         self._obj.dp.shuffle_by_file(seed)
         self._obj.dp.remove_night()
 
-        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data(seed=seed, num_inputs=para_num)
+        _, vdata, _, vtruth = self._obj.dp.get_ffn_training_data(seed=seed, input_type=para_num)
 
         extras = self._obj['confidence_an', 'bayes_in', 'cloud_an']
         extras_array = extras.values
@@ -341,7 +341,7 @@ class DataAnalyser():
             b = surftype_list[i]
 
             if len(b) > 0:
-                acc = me.get_accuracy(model.model, b[:, 0], b[:, 1])
+                acc = me.get_accuracy(model.model, b[:, 0], b[:, 1], para_num=para_num)
                 bayes_mask = b[:, 2]
                 emp_mask = b[:, 3]
                 bayes_mask[bayes_mask > 1.0] = 1.0
@@ -376,14 +376,14 @@ class DataAnalyser():
                                             'Empirical mask accuracy'])
         plt.show()
 
-    def reproducibility(self, model, number_of_runs=15):
+    def reproducibility(self, model, number_of_runs=15, para_num=22):
 
         accuracies = []
 
         for i in range(number_of_runs):
-            tdata, vdata, ttruth, vtruth = self._obj.dp.get_ffn_training_data()
+            tdata, vdata, ttruth, vtruth = self._obj.dp.get_ffn_training_data(input_type=para_num)
             model.Train(tdata, ttruth, vdata, vtruth)
-            acc = me.get_accuracy(model, vdata, vtruth, para_num=24)
+            acc = me.get_accuracy(model, vdata, vtruth, para_num=para_num)
             accuracies.append(acc)
 
         average = np.mean(accuracies)

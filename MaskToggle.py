8c2a821d904c1e67a17013840bd36bee6c393110
Tom Zhu <tomzhu1@gmail.com>
2019-02-21 22:51:35 +0000

    Added additional options for toggling

--- MaskToggle.py
+++ MaskToggle.py
@@ -1,38 +1,94 @@
-from ModelApplication import apply_mask
+import matplotlib.pyplot as plt
+
 import DataLoader as DL
 import Visualisation as Vis
 from FFN import FFN
 
-import matplotlib.pyplot as plt
-import numpy as np
+Sfile = r"./SatelliteData/SLSTR/Poster/S3A_SL_1_RBT____20180531T222736_20180531T223036_20180602T040456_0179_032_001_1800_LN2_O_NT_003.SEN3"
 
-model = FFN('Net1_FFN_v7')
+modelname = 'Net1_FFN_v7'
+model = FFN(modelname)
 model.Load()
 
-Sfile = r"C:\SLSTR\S3A_SL_1_RBT____20180703T235729_20180704T000029_20180704T012919_0179_033_087_4500_SVL_O_NR_003.SEN3"
+mask1 = model.apply_mask(Sfile)[0]
 
-mask1 = apply_mask(model.model, Sfile, input_type=22)[0]
-mask1 = 1 - mask1
-
-# two images x1 is initially visible, x2 is not
-
-rgb = Vis.FalseColour(Sfile, False)[0]
+rgb, TitleStr = Vis.FalseColour(Sfile, False)
 
 im1 = plt.imshow(rgb)
+plt.title('False colour image\n' + TitleStr)
+
 im2 = plt.imshow(mask1, cmap='Blues')
 im2.set_visible(False)
 
+bmask = DL.extract_mask(Sfile, 'bayes_in', 2)
+im3 = plt.imshow(bmask, cmap='Reds')
+im3.set_visible(False)
 
-def toggle_images(event):
-    'toggle the visible state of the two images'
-    if event.key != 't':
-        return
-    b1 = im1.get_visible()
-    b2 = im2.get_visible()
-    im1.set_visible(not b1)
-    im2.set_visible(not b2)
-    plt.draw()
+mask1 = mask1.astype('bool')
+rgb[~mask1, 0] = 254 / 255
+rgb[~mask1, 1] = 253 / 255
+rgb[~mask1, 2] = 185 / 255
+im4 = plt.imshow(rgb)
+im4.set_visible(False)
 
-plt.connect('key_press_event', toggle_images)
+
+# TODO Refactor
+class Toggler():
+    def __init__(self):
+        self.setting = 1
+
+    def toggle_images(self, event):
+        """Toggle between different images to display"""
+        if event.key == '1':
+            self.setting1()
+        elif event.key == '2':
+            self.setting2()
+        elif event.key == '3':
+            self.setting3()
+        elif event.key == '4':
+            self.setting4()
+        else:
+            return
+
+    def setting1(self):
+        plt.title('False colour image\n' + TitleStr)
+        im1.set_visible(True)
+        im2.set_visible(False)
+        im3.set_visible(False)
+        im4.set_visible(False)
+        self.setting = 1
+        plt.draw()
+
+    def setting2(self):
+        plt.title(modelname + ' mask\n' + TitleStr)
+        im1.set_visible(False)
+        im2.set_visible(True)
+        im3.set_visible(False)
+        im4.set_visible(False)
+        self.setting = 2
+        plt.draw()
+
+    def setting3(self):
+        plt.title('Bayesian mask\n' + TitleStr)
+        im1.set_visible(False)
+        im2.set_visible(False)
+        im3.set_visible(True)
+        im4.set_visible(False)
+        self.setting = 3
+        plt.draw()
+
+    def setting4(self):
+        plt.title(modelname + ' masked false colour image\n' + TitleStr)
+        im1.set_visible(False)
+        im2.set_visible(False)
+        im3.set_visible(False)
+        im4.set_visible(True)
+        self.setting = 4
+        plt.draw()
+
+
+toggler = Toggler()
+
+plt.connect('key_press_event', toggler.toggle_images)
 
 plt.show()

4cfaae9a000fb9e8a94d3ef59e5d6da8e26e9d09
Tom Zhu <tomzhu1@gmail.com>
2019-03-04 23:27:42 +0000

    MaskSLSTR now outputs the same figures as MaskToggle

--- MaskSLSTR.py
+++ MaskSLSTR.py
@@ -5,37 +5,56 @@
 # Licence version 3 (GPLv3)
 ##############################################
 
+import sys
+
+import matplotlib.pyplot as plt
+
 import DataLoader as DL
 import Visualisation as Vis
 from FFN import FFN
 
-model = FFN('Net1_FFN_v4')
+model = FFN('Net1_FFN_v7')
 model.Load()
+modelname = model.name
 
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        Sfilename = r"./SatelliteData/SLSTR/2018/05/S3A_SL_1_RBT____20180531T222736_20180531T223036_20180602T040456_0179_032_001_1800_LN2_O_NT_003.SEN3"
+    else:
+        Sfilename = sys.argv[1]
 
-# Pacific Ocean
-# Sfile = "./SatelliteData/SLSTR/Datas/t1\S3A_SL_1_RBT____20180528T190110_20180528T190410_20180529T235914_0179_031_341_2880_LN2_O_NT_003.SEN3"
-# Sfile = "./SatelliteData/SLSTR/Datas/t1\S3A_SL_1_RBT____20180817T190106_20180817T190406_20180819T002616_0179_034_341_2880_LN2_O_NT_003.SEN3"
+    mask1, pmask = model.apply_mask(Sfilename)
 
-# NE Australia
-# Sfile = "./SatelliteData/SLSTR/Datas/t1\S3A_SL_1_RBT____20180822T000319_20180822T000619_20180823T043035_0179_035_016_3060_LN2_O_NT_003.SEN3"
-# Sfile = "./SatelliteData/SLSTR/Datas/t1\S3A_SL_1_RBT____20180822T000619_20180822T000919_20180822T015223_0179_035_016_3240_SVL_O_NR_003.SEN3"
+    rgb, TitleStr = Vis.FalseColour(Sfilename, False)
 
-# Mainland Australia
-# Sfile = "./SatelliteData/SLSTR/Datas/t1\S3A_SL_1_RBT____20180822T000919_20180822T001219_20180822T015515_0179_035_016_3420_SVL_O_NR_003.SEN3"
+    plt.figure()
+    plt.imshow(rgb)
+    plt.title('False colour image\n' + TitleStr)
 
-Sfiles = ["./SatelliteData/SLSTR/Test4/S3A_SL_1_RBT____20180212T103733_20180212T104033_20180213T142548_0180_027_379_1620_LN2_O_NT_002.SEN3",
-          "./SatelliteData/SLSTR/Test4/S3A_SL_1_RBT____20180503T041955_20180503T042255_20180504T103326_0180_030_361_1620_LN2_O_NT_003.SEN3",
-          "./SatelliteData/SLSTR/Test4/S3A_SL_1_RBT____20180908T072307_20180908T072607_20180909T124616_0179_035_263_1620_LN2_O_NT_003.SEN3",
-          "./SatelliteData/SLSTR/Test4/S3A_SL_1_RBT____20180911T010137_20180911T010437_20180912T055246_0179_035_302_1620_LN2_O_NT_003.SEN3",
-          "./SatelliteData/SLSTR/Test4/S3A_SL_1_RBT____20181004T225815_20181004T230115_20181006T034958_0179_036_258_1620_LN2_O_NT_003.SEN3"]
+    plt.figure()
+    plt.imshow(mask1, cmap='Blues')
+    plt.title(modelname + ' mask\n' + TitleStr)
 
+    plt.figure()
+    bmask = DL.extract_mask(Sfilename, 'bayes_in', 2)
+    im3 = plt.imshow(bmask, cmap='Reds')
+    plt.title('Bayesian mask\n' + TitleStr)
 
-for Sfile in Sfiles:
+    plt.figure()
+    mask1 = mask1.astype('bool')
+    rgb[~mask1, :] = 254 / 255, 253 / 255, 185 / 255
+    im4 = plt.imshow(rgb)
+    plt.title(modelname + ' masked false colour image\n' + TitleStr)
 
-    mask1 = model.apply_mask(Sfile)[0]
+    plt.figure()
+    im5 = plt.imshow(1 - pmask, cmap='Oranges')
+    plt.title(modelname + ' model output\n' + TitleStr)
+    plt.colorbar(im5)
 
-    # bmask = DL.extract_mask(Sfile, 'cloud_an', 64)
-    bmask = DL.extract_mask(Sfile, 'bayes_in', 2)
+    plt.figure()
+    maskdiff = bmask - mask1
+    im6 = plt.imshow(maskdiff, cmap='bwr')
+    plt.title(modelname + ' mask - Bayesian mask\n' + TitleStr)
+    plt.colorbar(im6)
 
-    Vis.MaskComparison(Sfile, mask1, bmask, True, 1000)
+    plt.show()

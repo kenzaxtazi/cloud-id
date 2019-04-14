b41ed654b77a91bab385100e640f3db181bb8d0e
Kenza Tazi <kt2015@ic.ac.uk>
2019-04-14 18:20:38 +0100

    saves as val acc to pickle file

--- HyperparameterTesting.py
+++ HyperparameterTesting.py
@@ -4,24 +4,29 @@
 # Licence version 3 (GPLv3)
 ##############################################
 
-from FFN import FFN
+from FFNtest import FFN
 from tqdm import tqdm
 import DataPreparation as dp
 import tensorflow as tf
+import ModelEvaluation as me 
+import pandas as pd
 
 
-# Correlation test
+# This is a script to get some visulisation of unbias hyperparameter testing. 
 
 df = dp.PixelLoader('./SatelliteData/Pixels3')
 
 tdata, vdata, ttruth, vtruth = df.dp.get_ffn_training_data(21)
 
-epochs = [10, 50, 100, 150]
-neurons = [16, 32, 64, 128]
-LRs = [1e-1, 1e-2, 1e-3, 1e-4]
+
+epochs = 10
+neurons = [32, 16 , 64, 128]
+LRs = [1e-3, 1e-1, 1e-2, 1e-4]
 hidden_layers = [4, 8, 16, 32]
-batch_size = [16, 32, 64, 128]
-dropout = [0.2, 0.4, 0.6, 0.8]
+batch_size = [64, 16, 32, 128]
+dropout = [0.8, 0.2, 0.4, 0.6]
+
+hyp_df = pd.DataFrame(columns=['epochs', 'neurons', 'hidden_layers', 'batch_size', 'LR', 'dropout', 'val_acc'])
 
 
 for e in epochs:
@@ -36,6 +41,11 @@ for e in epochs:
                                     epochs=e, dout=do)
                         model.Train(tdata, ttruth, vdata, vtruth)
                         model.Save()
+                        val_acc = me.get_accuracy(model, vdata, vtruth)
+                        hyp_df = hyp_df.append({'epochs': e, 'neurons':n, 
+                                                'hidden_layers':hl, 'batch_size':bs,
+                                                'LR':lr, 'dropout':do, 'val_acc': val_acc}, 
+                                                ignore_index=True)
                         tf.reset_default_graph()
 
-# Further investigation
+df.to_pickle('hyp_test')

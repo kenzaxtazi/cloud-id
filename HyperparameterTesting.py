392a776cb207ada93afc525e83c7ccb648e4d07e
Kenza Tazi <kt2015@ic.ac.uk>
2019-04-18 12:46:17 +0100

    put seed

--- HyperparameterTesting.py
+++ HyperparameterTesting.py
@@ -20,13 +20,15 @@ from scipy.interpolate import interp2D
 
 df = dp.PixelLoader('./SatelliteData/Pixels3')
 
-tdata, vdata, ttruth, vtruth = df.dp.get_ffn_training_data(21)
+tdata, vdata, ttruth, vtruth = df.dp.get_ffn_training_data(21, seed=2553149187)
 
-"""
+'''
+# for layers and neurons 
+
 epochs = [1]
 neurons = [32, 16, 64, 128, 254, 512, 1024]
 LRs = [1e-3]  # , 1e-1, 1e-2, 1e-4]
-hidden_layers = [4, 8, 16]
+hidden_layers = [2, 4, 8, 16, 32, 64, 128]
 batch_size = [64]  # 16, 32, 128]
 dropout = [0.8]  # , 0.2, 0.4, 0.6]
 
@@ -51,9 +53,42 @@ for e in epochs:
                         model.Save()
                         tf.reset_default_graph()
 
-hyp_df.to_pickle('hyp_test')
-"""
+hyp_df.to_pickle('layers_neurons_test')
+'''
 
+# for learning rate and dropout
+
+epochs = [1]
+neurons = [128]
+LRs = [ 1e-1, 0.05, 1e-2, 0.005, 1e-3, 0.0005, 1e-4]
+hidden_layers = [8]
+batch_size = [64]  # 16, 32, 128]
+dropout = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+hyp_df = pd.DataFrame(columns=['epochs', 'neurons', 'hidden_layers', 'batch_size', 'LR', 'dropout', 'val_acc'])
+
+for e in epochs:
+    for bs in batch_size:
+        for lr in tqdm(LRs):
+            for do in dropout:
+                for hl in tqdm(hidden_layers):
+                    for n in neurons:
+                        model = FFN(str(e) + '_' + str(bs) + '_' + str(lr) + '_' +
+                                    str(do) + '_' + str(hl) + '_' + str(n), 'TestNetwork', 21, LR=lr,
+                                    neuron_num=n, hidden_layers=hl, batch_size=bs,
+                                    epochs=e, dout=do)
+                        model.Train(tdata, ttruth, vdata, vtruth)
+                        val_acc = me.get_accuracy(model.model, vdata, vtruth)
+                        hyp_df = hyp_df.append({'epochs': e, 'neurons': n,
+                                                'hidden_layers': hl, 'batch_size': bs,
+                                                'LR': lr, 'dropout': do, 'val_acc': val_acc}, 
+                                               ignore_index=True)
+                        model.Save()
+                        tf.reset_default_graph()
+
+hyp_df.to_pickle('lr_dropout_test')
+
+'''
 # Fig 2
 
 fig = plt.figure()
@@ -88,3 +123,4 @@ ax.set_zlabel('Accuracy')
 # Add a color bar which maps values to colors.
 fig.colorbar(surf)
 plt.show()
+'''
